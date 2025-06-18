a99fc5f5f89a725dc5525c5cfad985baed4b14ab
JonahThurston <jonah.m.thurston@gmail.com>
2025-06-18 16:12:17 -0600

    Finishe step 4

--- src/app/exercise/steps/step-four/step-four.component.ts
+++ src/app/exercise/steps/step-four/step-four.component.ts
@@ -1,15 +1,39 @@
-import { Component, model } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
+import { ValuesManagerService } from '../../service/values-manager.service';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { ValueBucket } from '../../service/value-bucket';
+import { ConfirmationDialogueComponent } from '../../confirmation-dialogue/confirmation-dialogue.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-step-four',
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './step-four.component.html',
   styleUrl: './step-four.component.css'
 })
 export class StepFourComponent {
   stepNumber = model(4);
+  readonly dialog = inject(MatDialog)
 
   proceedToNextStep(){
-    this.stepNumber.update(oldValue => oldValue + 1);
+    const reviewRef = this.dialog.open(ConfirmationDialogueComponent, {
+      data: {message: 'Are you ready to lock in your value ordering?'}
+    });
+          
+    reviewRef.afterClosed().subscribe(result => {
+      if (result === "confirm"){
+        this.stepNumber.update(oldValue => oldValue + 1);
+      }
+    });
+  }
+
+  valuesService = inject(ValuesManagerService);
+  allBuckets = this.valuesService.getBucketSignal();
+
+  drop(event: CdkDragDrop<ValueBucket[]>) {
+    this.allBuckets.update((bucketArray) => {
+      moveItemInArray(bucketArray, event.previousIndex, event.currentIndex)
+      return bucketArray
+    })
   }
 }
