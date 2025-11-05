2271d163ddcf5a7b6139a4aaa3cf2bfe60100b01
JonahThurston <jonah.m.thurston@gmail.com>
2025-11-05 09:55:11 -0700

    change timers

--- src/app/exercise/step-template/step-template.component.ts
+++ src/app/exercise/step-template/step-template.component.ts
@@ -15,73 +15,82 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-step-template',
-  imports: [RouterModule, TimerComponent, StepOneComponent, StepTwoComponent, StepThreeComponent, StepFourComponent, PodiumComponent, MatButtonModule, MatIconModule],
+  imports: [
+    RouterModule,
+    TimerComponent,
+    StepOneComponent,
+    StepTwoComponent,
+    StepThreeComponent,
+    StepFourComponent,
+    PodiumComponent,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './step-template.component.html',
-  styleUrl: './step-template.component.css'
+  styleUrl: './step-template.component.css',
 })
 export class StepTemplateComponent {
   stepNumber = signal(1);
 
-  readonly dialog = inject(MatDialog)
+  readonly dialog = inject(MatDialog);
   @ViewChild(TimerComponent) timer!: TimerComponent;
 
-  constructor(){
+  constructor() {
     effect(() => {
       const step = this.stepNumber();
-      if (step > 1 && step < 5 && this.timer){
+      if (step > 1 && step < 5 && this.timer) {
         this.setNewTimer();
         this.openInstructions();
       }
     });
   }
-  
+
   ngOnInit(): void {
     const introRef = this.dialog.open(StepInstructionsComponent, {
-      data: {stepNumber: this.stepNumber()}
+      data: { stepNumber: this.stepNumber() },
     });
 
-    introRef.afterClosed().subscribe(result => {
+    introRef.afterClosed().subscribe((result) => {
       //console.log(`Dialog result: ${result}`);
-      this.timer.reset(7);
+      this.timer.reset(5);
       this.timer.start();
     });
   }
 
-  setNewTimer(): void{
-    switch(this.stepNumber()){
-      case(1) : {
-        this.timer.reset(7);
-        break;
-      }
-      case(2) : {
-        this.timer.reset(10);
-        break;
-      }
-      case(3) : {
+  setNewTimer(): void {
+    switch (this.stepNumber()) {
+      case 1: {
         this.timer.reset(5);
         break;
       }
-      case(4) : {
+      case 2: {
+        this.timer.reset(7);
+        break;
+      }
+      case 3: {
+        this.timer.reset(5);
+        break;
+      }
+      case 4: {
         this.timer.reset(2);
         break;
       }
-      default : {
+      default: {
         this.timer.reset(7);
       }
     }
   }
 
-  openInstructions(): void{
+  openInstructions(): void {
     this.timer.stop();
-    
+
     const introRef = this.dialog.open(StepInstructionsComponent, {
-      data: {stepNumber: this.stepNumber()}
+      data: { stepNumber: this.stepNumber() },
     });
 
-    introRef.afterClosed().subscribe(result => {
+    introRef.afterClosed().subscribe((result) => {
       //console.log(`Dialog result: ${result}`);
       this.timer.start();
     });
   }
-
 }
--- src/app/exercise/timer/timer.component.ts
+++ src/app/exercise/timer/timer.component.ts
@@ -4,41 +4,38 @@ import { Component, OnDestroy } from '@angular/core';
   selector: 'app-timer',
   imports: [],
   template: '<p class="timer-text">{{getTimeString()}}</p>',
-  styleUrl: './timer.component.css'
+  styleUrl: './timer.component.css',
 })
-export class TimerComponent implements OnDestroy{
-  minutesLeft: number = 7;
+export class TimerComponent implements OnDestroy {
+  minutesLeft: number = 5;
   secondsLeft: number = 0;
-  intervalId: any; //This is really supposed to be Nodejs.Timeout, but I dont care to figure that out. 
+  intervalId: any; //This is really supposed to be Nodejs.Timeout, but I dont care to figure that out.
   timesUp: boolean = false;
 
   ngOnDestroy() {
     this.stop();
   }
 
-  start(): void{
+  start(): void {
     this.intervalId = setInterval(() => {
-      if(!this.timesUp){
+      if (!this.timesUp) {
         this.secondsLeft--;
 
-        if (this.secondsLeft < 0){
+        if (this.secondsLeft < 0) {
           this.minutesLeft--;
 
-          if(this.minutesLeft < 0){
+          if (this.minutesLeft < 0) {
             this.secondsLeft = 0;
-            this.minutesLeft = 0
+            this.minutesLeft = 0;
             this.timesUp = true;
-          }
-          else{
+          } else {
             this.secondsLeft = 59;
           }
         }
-      } 
-
-      else {
+      } else {
         this.secondsLeft++;
 
-        if(this.secondsLeft > 59){
+        if (this.secondsLeft > 59) {
           this.minutesLeft++;
           this.secondsLeft = 0;
         }
@@ -46,20 +43,20 @@ export class TimerComponent implements OnDestroy{
     }, 1000);
   }
 
-  stop(): void{
+  stop(): void {
     clearInterval(this.intervalId);
   }
 
-  reset(minutes: number): void{
+  reset(minutes: number): void {
     this.minutesLeft = minutes;
     this.secondsLeft = 0;
   }
 
-  getTimeString(): string{
+  getTimeString(): string {
     const timesUpPrefix = this.timesUp ? '-' : '';
     const minutes = this.minutesLeft.toString().padStart(2, '0');
     const seconds = this.secondsLeft.toString().padStart(2, '0');
 
-    return `${timesUpPrefix}${minutes}:${seconds}`
+    return `${timesUpPrefix}${minutes}:${seconds}`;
   }
 }
