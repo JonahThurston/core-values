596b3b4a47cfca486e3c46ff450bde733409843b
JonahThurston <jonah.m.thurston@gmail.com>
2025-11-13 13:26:35 -0700

    Add start confirmation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { ConfirmationDialogueComponent } from '../exercise/confirmation-dialogue/confirmation-dialogue.component';
 
 @Component({
   selector: 'app-home',
   imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  readonly dialog = inject(MatDialog);
+  private router = inject(Router);
 
+  handleStartClick() {
+    const reviewRef = this.dialog.open(ConfirmationDialogueComponent, {
+      data: {
+        message:
+          'Are you ready to start the exercise? You will not be able to refresh your browser window or push its back button, or you may lose progress.',
+      },
+    });
+
+    reviewRef.afterClosed().subscribe((result) => {
+      if (result === 'confirm') {
+        this.router.navigate(['/exercise']);
+      }
+    });
+  }
 }
