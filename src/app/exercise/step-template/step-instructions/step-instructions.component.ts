e3855ee5fb4963e5261e7de246ec7c2f444b883f
JonahThurston <jonah.m.thurston@gmail.com>
2025-11-14 12:28:00 -0700

    initial set up of cancel buttons

--- src/app/exercise/step-template/step-instructions/step-instructions.component.ts
+++ src/app/exercise/step-template/step-instructions/step-instructions.component.ts
@@ -1,10 +1,16 @@
-import { ChangeDetectionStrategy, Component, inject, model } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  model,
+} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 
 import { StepOneInstructionsComponent } from '../../steps/step-one/step-one-instructions/step-one-instructions.component';
 import { StepTwoInstructionsComponent } from '../../steps/step-two/step-two-instructions/step-two-instructions.component';
 import { StepThreeInstructionsComponent } from '../../steps/step-three/step-three-instructions/step-three-instructions.component';
 import { StepFourInstructionsComponent } from '../../steps/step-four/step-four-instructions/step-four-instructions.component';
+import { MatIcon } from '@angular/material/icon';
 
 export interface DialogData {
   stepNumber: number;
@@ -12,7 +18,14 @@ export interface DialogData {
 
 @Component({
   selector: 'app-step-instructions',
-  imports: [MatDialogModule, StepOneInstructionsComponent, StepTwoInstructionsComponent, StepThreeInstructionsComponent, StepFourInstructionsComponent],
+  imports: [
+    MatDialogModule,
+    StepOneInstructionsComponent,
+    StepTwoInstructionsComponent,
+    StepThreeInstructionsComponent,
+    StepFourInstructionsComponent,
+    MatIcon,
+  ],
   templateUrl: './step-instructions.component.html',
   styleUrl: './step-instructions.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,5 +33,4 @@ export interface DialogData {
 export class StepInstructionsComponent {
   readonly data = inject<DialogData>(MAT_DIALOG_DATA);
   readonly stepNumber = model(this.data.stepNumber);
-
 }
